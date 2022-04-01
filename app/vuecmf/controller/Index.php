8c2c606cc02cfa9d6b40a97938f382be5f31e617
vuecmf <tulihua2004@126.com>
2022-04-01 20:35:02 +0800

    VueCMF内容管理快速开发框架v2.0.0（完整后端API）

---
+++ app/vuecmf/controller/Index.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | Copyright (c) 2019~2022 http://www.vuecmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( https://github.com/emei8/vuecmf/blob/master/LICENSE )
+// +----------------------------------------------------------------------
+// | Author: emei8 <2278667823@qq.com>
+// +----------------------------------------------------------------------
+declare (strict_types = 1);
+
+namespace app\vuecmf\controller;
+
+
+class Index
+{
+    public function index(): string
+    {
+        return 'Welcome to VueCMF!';
+    }
+
+}
