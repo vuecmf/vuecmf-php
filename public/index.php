a1852cf24dfba6561642ee3f90a11a1963b1b55c
vuecmf <tulihua2004@126.com>
2022-04-02 16:13:31 +0800

    入口文件增加域名跨域设置

--- public/index.php
+++ public/index.php
@@ -14,6 +14,17 @@ namespace think;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+//增加域名跨域设置
+$origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';
+$allow_origin = array(
+    'http://localhost:8080', //填写自己的前端网页域名
+);
+if(in_array($origin, $allow_origin)){
+    header('Access-Control-Allow-Origin:'.$origin);
+    header('Access-Control-Allow-Credentials:true');
+    header('Access-Control-Allow-Headers:Content-Type, token');
+}
+
 // 执行HTTP应用并响应
 $http = (new App())->http;
 
