9a075a235acb32e07515e1e67f5bb99b540cb29d
vuecmf <tulihua2004@126.com>
2023-08-19 17:12:27 +0800

    增加跨域中间件、跨域设置调整在.env文件中配置

--- app/vuecmf/controller/Index.php
+++ app/vuecmf/controller/Index.php
@@ -8,7 +8,7 @@ class Index
 {
     public function index(): string
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) </h1><p>Welcome to VueCMF V2.1<br/></p><span style="font-size:25px;">[ Powered by <a href="http://www.vuecmf.com/" target="_blank">vuecmf</a> ]</span><script src="https://hm.baidu.com/hm.js?74079f71bcec1421dd89f7c08ed21d68"></script></div>';
+        return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) </h1><p>Welcome to VueCMF V2.4<br/></p><span style="font-size:25px;">[ Powered by <a href="http://www.vuecmf.com/" target="_blank">vuecmf</a> ]</span><script src="https://hm.baidu.com/hm.js?74079f71bcec1421dd89f7c08ed21d68"></script></div>';
     }
 }
         
--- app/vuecmf/middleware.php
+++ app/vuecmf/middleware.php
@@ -1,6 +1,7 @@
 <?php
-// 这是系统自动生成的middleware定义文件
 return [
     // Session初始化
     \think\middleware\SessionInit::class,
+    //跨域设置
+    \app\vuecmf\middleware\CrossDomain::class
 ];
